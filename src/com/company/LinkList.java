b6e93d959eae2ff12a0c415cccfc82afb8f793d2
TokunR <tokunogunmola@gmail.com>
2022-05-30 23:04:58 -0400

    Data Structures and Algorithms Examples

--- src/com/company/LinkList.java
+++ src/com/company/LinkList.java
@@ -1,5 +1,9 @@
 package com.company;
 
+
+
+import java.util.NoSuchElementException;
+
 public class LinkList {
     private class Node{
         private int value;
@@ -12,23 +16,86 @@ public class LinkList {
     private Node first;
     private Node last;
 
-    // addFirst
-    public void addFirst(int item){
+    // addLast
+    public void addLast(int item){
         Node node = new Node(item);
         //node.value = item;
         if (isEmpty())
             first = last = node;
         else{
-            node.next = node;
+            last.next = node;
+            last = node;
+        }
+    }
+    // addFirst
+    public void addFirst (int item){
+        Node node = new Node (item);
+
+        if (isEmpty())
+            first = last = node;
+        else{
+            node.next = first;
             first = node;
         }
+
     }
     private boolean isEmpty(){
         return first ==null;
     }
-    // addLast
-    // deleteFirst
-    // deleteLast
-    // contains
+
     // indexOf
+    public int indexOf(int item) {
+        int index = 0;
+        Node current = first;
+        while (current != null) {
+            if (current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    // contains
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    // deleteFirst
+    public void removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+        Node second = first.next;
+        first.next = null;
+        first = second;
+    }
+        // deleteLast
+        public void removeLast(){
+            if (isEmpty())
+                throw new NoSuchElementException();
+            if (first == last) {
+                first = last = null;
+                return;
+            }
+
+            Node previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+        private Node getPrevious (Node node){
+            Node current = first;
+            while (current != null) {
+                if (current.next == last) return current;
+                current = current.next;
+            }
+            return null;
+
+    }
+
+
+
+ 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
+
         list.addLast(10);
         list.addLast(20);
         list.addLast(30);
+        list.removeLast();
+        System.out.println(list.contains(10));
+
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addFirst(30);
 
 
 
